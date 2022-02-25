ba9a2311a743fea12ca00c161363f55fffff9b08
TheNewKid956 <11danilatros@gmail.com>
2022-02-25 07:52:47 -0600

    Added Project to Repo

---
+++ Brick Breaker/Assets/Scripts/BottomWallCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomWallCollider : MonoBehaviour
+{
+    public LevelManager levelManager;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        levelManager.loadLevel("lose");
+    }
+}
